868509c323c1dad4b66079124161683129343b3a
wcamer <xeus.xiii@hotmail.com>
2025-08-17 22:42:54 -0600

    More changes

--- HogwartsBattle.Server/obj/Debug/net8.0/HogwartsBattle.Server.AssemblyInfo.cs
+++ HogwartsBattle.Server/obj/Debug/net8.0/HogwartsBattle.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HogwartsBattle.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0a2dcd2fc368887e7dec78cc4903feb371beea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf028e12999a8b4aaa30007167bf6537c6e5a63")]
 [assembly: System.Reflection.AssemblyProductAttribute("HogwartsBattle.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HogwartsBattle.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
